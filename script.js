1b79f5356821fde601c2ef7e1cc56f2bb8aadbaf
isacilivi <149582051+isacilivi@users.noreply.github.com>
2025-09-08 11:48:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.addEventListener('DOMContentLoaded',function () {
+    // Utilitário para formatar moeda (R$)
+    function moedaBR(valor) {
+        return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(valor);
+        }
+
+        // normaliza entrada (troca vírgula por ponto e coverte para número) function toNumber(val) {
+        if (typeof val === 'number') return val;
+        if (!val )}
+    }
+}
 
