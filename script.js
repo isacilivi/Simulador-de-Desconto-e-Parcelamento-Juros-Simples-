8abcc1d0a22f79342b402cb7118f5806d73695cc
isacilivi <149582051+isacilivi@users.noreply.github.com>
2025-09-11 08:00:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,20 @@ document.addEventListener('DOMContentLoaded',function () {
 
         // normaliza entrada (troca vírgula por ponto e coverte para número) function toNumber(val) {
         if (typeof val === 'number') return val;
-        if (!val )}
+        if (!val && !== 0) return NaN;
+        return parseFloat(String(val).trim().replace(',', '.'));
     }
+    const form = document.getElementById('form');
+    const error = document.getElementById('erro');
+    const resultados = document.getElementById('resultados');
+    const tanelaSecao = document.getElementById('tabelaSecao');
+
+    const outPrecoComDesconto = document.getElementById('precoComDesconto');
+    const outValorParcela = document.getElementById('valorParcela');
+    const outTotalPagar = document.getElementById('totalPagar');
+    const outEconomia = document.getElementById('economia');
+
+    if (!form) {
+        console.error('form não encontrado (id="form"). Verifique o HTML.');return;                          
 }
 
